f101de1ce5b862f75e12b6acd1652df626318902
nara <parknara0213@gmail.com>
2024-02-04 15:42:22 +0900

    240204

--- study/41-dynamic-values.jsx
+++ study/41-dynamic-values.jsx
@@ -1,7 +1,9 @@
-import reactImg from "./assets/react-core-concepts.png";
-
+// dynamic value
+// if-statements, for-loops, function definitions and other block statements are not allowed here!
+// Only expressions that directly produce a value
 const reactDescription = ["Fundamental", "Crucial", "Core"];
 
+// generate a random number
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
@@ -10,7 +12,7 @@ function Header() {
   const description = reactDescription[genRandomInt(2)];
   return (
     <header>
-      <img src={reactImg} alt="Stylized atom" />
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
