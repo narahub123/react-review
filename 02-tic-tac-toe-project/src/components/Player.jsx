32fcfb6f990ddef71bdcf6f966697e6ec60ac761
nara <parknara0213@gmail.com>
2024-02-16 15:23:29 +0900

    240216 pre-populate

--- 02-tic-tac-toe-project/src/components/Player.jsx
+++ 02-tic-tac-toe-project/src/components/Player.jsx
@@ -10,7 +10,7 @@ export default function Player({ name, symbol, onClick }) {
   let playerName = <span className="player-name">{name}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" value={name} required />;
   }
 
   return (
