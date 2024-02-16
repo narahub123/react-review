95d2e8225517bfd3f1506e73045284364667f8a4
nara <parknara0213@gmail.com>
2024-02-16 17:06:15 +0900

    240216 mulit-dimensional lists

---
+++ 02-tic-tac-toe-project/src/components/GameBoard.jsx
@@ -0,0 +1,22 @@
+export default function GameBoard() {
+  const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
