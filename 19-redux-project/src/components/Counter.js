5840ca9db7e400a06f5f56afd2e2a3f507d54523
nara <parknara0213@gmail.com>
2024-04-12 21:48:26 +0900

    240412 how to get data from store in react

--- 19-redux-project/src/components/Counter.js
+++ 19-redux-project/src/components/Counter.js
@@ -1,12 +1,17 @@
-import classes from './Counter.module.css';
+import { useSelector, connect } from "react-redux";
+// useSelector : allows us to automatically select a part of our state managed by the store
+// connect : used as a wrapper around the class component to connect that class component to store
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter); // automatically set up a subscription to Redux store for the component
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
