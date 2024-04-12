511c195c476bb8affafdabb853dc9f8f9ff6fffe
nara <parknara0213@gmail.com>
2024-04-13 06:42:42 +0900

    240413 practice redux and redux toolkit

--- 20-advanced-redux/src/components/Cart/CartItem.js
+++ 20-advanced-redux/src/components/Cart/CartItem.js
@@ -1,27 +1,49 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
+
+import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+  const { title, quantity, total, price, id } = props.item;
 
+  const removeItemFromCart = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addItemToCart = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
   return (
-    <li className={classes.item}>
-      <header>
-        <h3>{title}</h3>
-        <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+    <>
+      <li className={classes.item}>
+        <header>
+          <h3>{title}</h3>
+          <div className={classes.price}>
+            ${total.toFixed(2)}{" "}
+            <span className={classes.itemprice}>
+              (${price.toFixed(2)}/item)
+            </span>
+          </div>
+        </header>
+        <div className={classes.details}>
+          <div className={classes.quantity}>
+            x <span>{quantity}</span>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={removeItemFromCart}>-</button>
+            <button onClick={addItemToCart}>+</button>
+          </div>
         </div>
-      </header>
-      <div className={classes.details}>
-        <div className={classes.quantity}>
-          x <span>{quantity}</span>
-        </div>
-        <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
-        </div>
-      </div>
-    </li>
+      </li>
+    </>
   );
 };
 
