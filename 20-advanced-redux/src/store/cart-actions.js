c0a3f49655e0ec1e8bc19eaa826e131f9b71f032
nara <parknara0213@gmail.com>
2024-04-13 13:19:39 +0900

    240413 prevent sending data when fetching data

--- 20-advanced-redux/src/store/cart-actions.js
+++ 20-advanced-redux/src/store/cart-actions.js
@@ -45,7 +45,13 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(
         "https://real-time-database-4e094-default-rtdb.firebaseio.com/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
 
       if (!response.ok) {
